b2aa2cac23b4d3c8c42e245d394dac8738472214
Yashaswi Anand <yash.kwr17@gmail.com>
2022-09-20 20:27:03 +0530

    adding heroku files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const port = 5005;
+const port = process.env.PORT || 5000;
 const app = express();
 const db = require('./config/mongoose');
 const bodyParser = require('body-parser');
